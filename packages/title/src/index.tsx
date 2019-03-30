0c8023e84c868bd6ab09d62e9f52da7a3d7eaa43
Kasper Mikiewicz <IderedPL@gmail.com>
2019-03-30 19:57:15 +0100

    feat(title): add extended title config

--- packages/title/src/index.tsx
+++ packages/title/src/index.tsx
@@ -1,14 +1,24 @@
 import * as React from "react"
 import {Head} from "@smashing/head"
 
-export const TitleContext = React.createContext("")
+export const TitleContext = React.createContext<{
+  title: string
+  separator?: string | null
+}>({
+  title: "",
+  separator: ""
+})
 
 export const Title = ({children}: {children?: React.ReactChild}) => {
-  const title = React.useContext(TitleContext)
+  const {title, separator = " = "} = React.useContext(TitleContext)
 
   return (
     <Head>
-      <title>{[children, title].filter(Boolean).join(" - ")}</title>
+      <title>
+        {[children, title]
+          .filter(Boolean)
+          .join(separator === null ? "" : separator)}
+      </title>
     </Head>
   )
 }
