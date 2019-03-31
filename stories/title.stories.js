98b2eaf8feb59e41a1fbd37f7381e616ceff9943
Kasper Mikiewicz <IderedPL@gmail.com>
2019-03-31 04:53:36 +0200

    chore(stories): group functional components

--- stories/title.stories.js
+++ stories/title.stories.js
@@ -2,4 +2,6 @@ import React from "react"
 import {storiesOf} from "@storybook/react"
 import {Title} from "@smashing/title/src/index"
 
-storiesOf("Title", module).add("set page title", () => <Title>Hello</Title>)
+storiesOf("Functional|Title", module).add("set page title", () => (
+  <Title>Hello</Title>
+))
