d206460c81d376b70f5a493b2d8ea52f895f416d
Kasper Mikiewicz <IderedPL@gmail.com>
2019-04-17 00:31:03 +0200

    chore(typography): label - remove props

--- packages/typography/src/components/label.tsx
+++ packages/typography/src/components/label.tsx
@@ -4,16 +4,14 @@ import {getTextStyle} from "../styles/text"
 
 export interface LabelProps {
   variant?: 300 | 400 | 500
-  color?: "muted" | "default" | "dark"
-  intent?: "success" | "info" | "danger" | "warning"
   fontFamily?: "ui" | "display" | "mono"
 }
 
 export const Label = styled.label<LabelProps>`
-  ${({variant = 400, fontFamily, color = "default", intent}) => css`
+  ${({variant = 400, fontFamily}) => css`
     ${getTextStyle(variant)}
-    color: ${getTextColor(intent || color)};
-    font-weight: 600;
+    color: ${getTextColor("dark")};
+    font-weight: 500;
     ${fontFamily &&
       css`
         font-family: ${getFontFamily(fontFamily)};
