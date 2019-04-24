b4d49000ee8af9994b2c93d78570dd4e4548bb28
Kasper Mikiewicz <IderedPL@gmail.com>
2019-04-24 11:50:51 +0200

    fix(popover): invalid typing

--- packages/popover/src/index.tsx
+++ packages/popover/src/index.tsx
@@ -42,7 +42,7 @@ export interface PopoverProps {
         toggle: () => void
         getRef: (ref: any) => void
         isShown: boolean
-      }) => Element | null)
+      }) => HTMLElement | null)
 
   /**
    * The display property passed to the Popover card.
