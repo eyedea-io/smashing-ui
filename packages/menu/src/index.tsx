ba54e2d9017ec38bd63340ef582e9106b1ffc9ea
Kasper Mikiewicz <IderedPL@gmail.com>
2019-04-24 13:16:56 +0200

    chore(menu): remove imports

--- packages/menu/src/index.tsx
+++ packages/menu/src/index.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import styled, {StyledComponent, DefaultTheme} from "styled-components"
+import styled from "styled-components"
 import {Text, Heading} from "@smashing/typography"
 
 const KeyCodes = {
