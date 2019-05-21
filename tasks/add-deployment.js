54f56194e5f505029274ca21fea20bd925a41fb1
Kasper Mikiewicz <IderedPL@gmail.com>
2019-05-21 18:08:04 +0200

    fix: deployment

--- tasks/add-deployment.js
+++ tasks/add-deployment.js
@@ -2,7 +2,7 @@
 const Octokit = require("@octokit/rest")
 const octokit = new Octokit({auth: process.env.GH_AUTH_TOKEN})
 
-const buildLink = (repoID, buildNum, pathToRepo) => {
+const buildTargetUrl = (repoID, buildNum, pathToRepo) => {
   return `https://${buildNum}-${repoID}-gh.circle-artifacts.com/0${pathToRepo}/storybook-static/index.html`
 }
 
@@ -12,19 +12,18 @@ const buildLink = (repoID, buildNum, pathToRepo) => {
     repo: process.env.CIRCLE_PROJECT_REPONAME
   })
 
-  const link = buildLink(
+  const targetUrl = buildTargetUrl(
     repo.data.id,
     process.env.CIRCLE_BUILD_NUM,
     process.env.CIRCLE_WORKING_DIRECTORY.replace("~", process.env.HOME)
   )
 
-  console.log("Link:", link)
-
   const deployment = await octokit.repos.createDeployment({
     environment: "staging",
     owner: process.env.CIRCLE_PROJECT_USERNAME,
     repo: process.env.CIRCLE_PROJECT_REPONAME,
-    ref: process.env.CIRCLE_SHA1
+    ref: process.env.CIRCLE_BRANCH,
+    task: "deploy"
   })
 
   octokit.repos.createDeploymentStatus({
@@ -32,14 +31,8 @@ const buildLink = (repoID, buildNum, pathToRepo) => {
     environment: "staging",
     owner: process.env.CIRCLE_PROJECT_USERNAME,
     repo: process.env.CIRCLE_PROJECT_REPONAME,
-    log_url: link,
+    target_url: targetUrl,
+    description: "Deployment has completed",
     state: "success"
   })
-
-  // await octokit.issues.createComment({
-  //   owner: process.env.CIRCLE_PROJECT_USERNAME,
-  //   repo: process.env.CIRCLE_PROJECT_REPONAME,
-  //   issue_number: process.env.CIRCLE_PULL_REQUEST.split("/").pop(),
-  //   body: `Link to: [Storybook](${link})`
-  // })
 })()
