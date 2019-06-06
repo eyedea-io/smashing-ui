ba02b9c6a261bc99d3e2b339e7d6aa3ad7db1efb
Kasper Mikiewicz <IderedPL@gmail.com>
2019-06-06 17:04:36 +0200

    fix: add url

--- tasks/deployment/end.sh
+++ tasks/deployment/end.sh
@@ -2,6 +2,7 @@
 
 set -eu
 
+build_num=${CIRCLE_BUILD_NUM}
 owner=${CIRCLE_PROJECT_USERNAME}
 repo=${CIRCLE_PROJECT_REPONAME}
 token=${GITHUB_DEPLOYMENTS_TOKEN:?"Missing GITHUB_TOKEN environment variable"}
@@ -11,10 +12,28 @@ if ! deployment_id=$(cat deployment); then
   exit 3
 fi
 
+if ! repository=$(curl -s \
+                  -X GET \
+                  -H "Authorization: bearer ${token}" \
+                  -d "{}" \
+                  -H "Content-Type: application/json" \
+                  "https://api.github.com/repos/${owner}/${repo}"); then
+  echo "Could not fetch repository data" 1>&2
+  exit 1
+fi
+
+if ! repository_id=$(echo "${repository}" | python -c 'import sys, json; print json.load(sys.stdin)["id"]'); then
+  echo "Could not extract repository ID from API response" 1>&2
+  exit 3
+fi
+
+path_to_repo=$(echo "$CIRCLE_WORKING_DIRECTORY" | sed -e "s:~:$HOME:g")
+url="https://${build_num}-${repository_id}-gh.circle-artifacts.com/0${path_to_repo}/storybook-static/index.html"
+
 if ! deployment=$(curl -s \
                   -X POST \
                   -H "Authorization: bearer ${token}" \
-                  -d "{\"state\": \"$1\", \"environment\": \"storybook\"}" \
+                  -d "{\"state\": \"$1\", \"environment\": \"storybook\", \"environment_url\": \"${url}\", \"target_url\": \"${url}\", \"log_url\": \"${url}\"}" \
                   -H "Content-Type: application/json" \
                   "https://api.github.com/repos/${owner}/${repo}/deployments/${deployment_id}/statuses" > res); then
   echo "POSTing deployment status failed, exiting (not failing build)" 1>&2
