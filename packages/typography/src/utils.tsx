ac30243a7d0b1d01fc9e4ccec82e0c9d9156e868
Jan Rochalski <joonny220@gmail.com>
2019-07-29 15:20:47 +0200

    fix(): fixes

--- packages/typography/src/utils.tsx
+++ packages/typography/src/utils.tsx
@@ -1,17 +1,8 @@
 import {DefaultTheme} from 'styled-components'
 import {themedProperty} from '@smashing/theme'
 
-export interface PaletteColor {
-  type: "lightest" | "light" | "base" | "dark"
-  color: string
-}
-
 export const getTextColor = (color: string) => (_: {theme: DefaultTheme}) =>
   themedProperty(_.theme.colors.text, color)
-
-export const getPaletteColor = ({color, type = "base"}: PaletteColor) => (_: {
-  theme: DefaultTheme
-}) => themedProperty(_.theme.palette, color)[type]
 
 export const getFontFamily = (fontFamily: string) => (_: {
   theme: DefaultTheme
