782beba2c30bd5aef53fc71804979871d94c614e
Kasper Mikiewicz <IderedPL@gmail.com>
2019-09-04 12:35:37 +0200

    chore(list): cleanup

--- packages/list/src/index.tsx
+++ packages/list/src/index.tsx
@@ -1,1 +1,36 @@
-export {List, ListProps} from './list'
+import * as React from 'react'
+import {useDefaults} from '@smashing/theme'
+import {ListContainer, ListItem} from './styles'
+
+export interface ListProps {
+  /**
+   * Children are expected to be a list of Elements
+   */
+  children: React.ReactNode[]
+  overlapSize: number
+}
+
+export const List: React.FC<ListProps> = ({children, ...props}) => {
+  const {overlapSize} = useDefaults('list', props, {
+    overlapSize: 50
+  })
+
+  return (
+    <ListContainer>
+      {children.map((child, key) => (
+        <ListItem key={key} overlapSize={overlapSize}>
+          {child}
+        </ListItem>
+      ))}
+    </ListContainer>
+  )
+}
+
+declare module 'styled-components' {
+  export interface SmashingListDefaults
+    extends Partial<{
+      list?: {
+        overlapSize?: number
+      }
+    }> {}
+}
