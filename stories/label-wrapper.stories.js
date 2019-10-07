f3188abf581792c11f4a6f45e75c4dcbb3e907b9
Nelias <michal.antropik@gmail.com>
2019-10-07 15:36:07 +0200

    feat(label-wrapper): add decorator and a proper input child in story

--- stories/label-wrapper.stories.js
+++ stories/label-wrapper.stories.js
@@ -1,14 +1,27 @@
 import React from 'react'
 import {storiesOf, addDecorator} from '@storybook/react'
 import {LabelWrapper} from '@smashing/label-wrapper'
+import {TextInput} from '@smashing/text-input'
 import {withA11y} from '@storybook/addon-a11y'
-// import {SmashingThemeProvider} from '@smashing/theme'
+import {SmashingThemeProvider} from '@smashing/theme'
 
 addDecorator(withA11y)
 
-storiesOf('Core|LabelWrapper', module).add('label', () => (
-  <LabelWrapper>
-    <span>Here</span>
-    <span>Here</span>
-  </LabelWrapper>
-))
+storiesOf('Core|LabelWrapper', module)
+  .addDecorator(story => (
+    <SmashingThemeProvider
+      theme={{
+        defaults: {},
+        fontFamilies: {
+          display: 'arial'
+        }
+      }}
+    >
+      {story()}
+    </SmashingThemeProvider>
+  ))
+  .add('label', () => (
+    <LabelWrapper labelTitle="Name">
+      <TextInput placeholder="Your name" />
+    </LabelWrapper>
+  ))
