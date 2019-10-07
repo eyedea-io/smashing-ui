e05846e30d3a0fe401ec73ca37959b81e76bc4c6
Nelias <michal.antropik@gmail.com>
2019-10-07 17:44:32 +0200

    feat(label-wrapper): update styles

--- packages/label-wrapper/src/styles.tsx
+++ packages/label-wrapper/src/styles.tsx
@@ -3,22 +3,25 @@ import styled from 'styled-components'
 export const Wrapper = styled.div`
   display: flex;
   position: relative;
+  background-color: white;
+  background: white;
 `
 export const Label = styled.span`
   display: flex;
   position: absolute;
   top: -10px;
   left: 12px;
-  background-color: white;
+  background-color: inherit;
+  background: inherit;
   padding: 0 5px;
 
-  font-family: Roboto, sans-serif;
+  font-family: Nunito, sans-serif;
   font-weight: 500;
   font-size: 12px;
 `
 
 export const Error = styled(Label)`
   top: inherit;
-  bottom: -8px;
+  bottom: -11px;
   color: #ff4752;
 `
