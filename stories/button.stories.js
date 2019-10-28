a4435878a8ff87b0cda3a642ed19acf5cbb8b291
Nelias <michal.antropik@gmail.com>
2019-10-28 10:21:15 +0100

    fix: remove flow-icons dependency

--- stories/button.stories.js
+++ stories/button.stories.js
@@ -3,7 +3,6 @@ import {storiesOf, addDecorator} from '@storybook/react'
 import {Button} from '@smashing/button'
 import {withA11y} from '@storybook/addon-a11y'
 import {SmashingThemeProvider} from '@smashing/theme'
-import {ArrowDown, Save, ArrowRight, Add} from '@eyedea/flow-icons'
 
 addDecorator(withA11y)
 
@@ -44,33 +43,37 @@ storiesOf('Core|Button', module)
   .add('appearance:default-with-icon', () => (
     <React.Fragment>
       <p>
-        <Button icon={Add} iconPosition="center" intent="info"></Button>
+        <Button
+          icon={ArrowRightRegular}
+          iconPosition="center"
+          intent="info"
+        ></Button>
       </p>
       <p>
-        <Button icon={ArrowDown} iconPosition="left" disabled>
+        <Button icon={ArrowRightRegular} iconPosition="left" disabled>
           Hulk smash!
         </Button>
       </p>
       <p>
-        <Button icon={ArrowDown}>Hulk smash!</Button>
+        <Button icon={ArrowRightRegular}>Hulk smash!</Button>
       </p>
       <p>
-        <Button icon={Save} intent="info">
+        <Button icon={ArrowRightRegular} intent="info">
           Hulk smash!
         </Button>
       </p>
       <p>
-        <Button icon={ArrowRight} intent="success">
+        <Button icon={ArrowRightRegular} intent="success">
           Hulk smash!
         </Button>
       </p>
       <p>
-        <Button icon={ArrowRight} intent="warning">
+        <Button icon={ArrowRightRegular} intent="warning">
           Hulk smash!
         </Button>
       </p>
       <p>
-        <Button icon={ArrowRight} intent="danger">
+        <Button icon={ArrowRightRegular} intent="danger">
           Hulk smash!
         </Button>
       </p>
@@ -236,3 +239,41 @@ storiesOf('Core|Button', module)
       Hulk smash!
     </Button>
   ))
+
+export var ArrowRightRegular = function(_a) {
+  var _b = _a.color,
+    color = _b === void 0 ? 'black' : _b,
+    _c = _a.size,
+    size = _c === void 0 ? 32 : _c
+  return React.createElement(
+    'svg',
+    {
+      width: size,
+      height: size,
+      viewBox: '0 0 16 16',
+      fill: 'none',
+      xmlns: 'http://www.w3.org/2000/svg'
+    },
+    React.createElement('path', {
+      fillRule: 'evenodd',
+      clipRule: 'evenodd',
+      d:
+        'M3 8C3 7.44772 3.44772 7 4 7H12C12.5523 7 13 7.44772 13 8C13 8.55228 12.5523 9 12 9H4C3.44772 9 3 8.55228 3 8Z',
+      fill: color
+    }),
+    React.createElement('path', {
+      fillRule: 'evenodd',
+      clipRule: 'evenodd',
+      d:
+        'M8.29289 4.29289C8.68342 3.90237 9.31658 3.90237 9.70711 4.29289L12.7071 7.29289C13.0976 7.68342 13.0976 8.31658 12.7071 8.70711C12.3166 9.09763 11.6834 9.09763 11.2929 8.70711L8.29289 5.70711C7.90237 5.31658 7.90237 4.68342 8.29289 4.29289Z',
+      fill: color
+    }),
+    React.createElement('path', {
+      fillRule: 'evenodd',
+      clipRule: 'evenodd',
+      d:
+        'M8.29289 11.7071C7.90237 11.3166 7.90237 10.6834 8.29289 10.2929L11.2929 7.29289C11.6834 6.90237 12.3166 6.90237 12.7071 7.29289C13.0976 7.68342 13.0976 8.31658 12.7071 8.70711L9.70711 11.7071C9.31658 12.0976 8.68342 12.0976 8.29289 11.7071Z',
+      fill: color
+    })
+  )
+}
