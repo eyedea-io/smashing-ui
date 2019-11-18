21c7af0a86e29793d8fe25d2ed5f0137cc5fdede
agata mroz <agata.mroz@pionerslab.com>
2019-11-18 10:55:16 +0100

    added custom scrollbar for outline textarea

--- packages/textarea/src/styled.tsx
+++ packages/textarea/src/styled.tsx
@@ -1,11 +1,38 @@
-import styled from 'styled-components'
+import styled, {DefaultTheme} from 'styled-components'
 import {getTextInputStyle} from '@smashing/text-input'
-import {TextareaProps} from './types'
+import {TextareaProps, TextareaAppearance} from './types'
 import {Text} from '@smashing/typography'
 import {getValueWithUnit} from '@smashing/theme'
 
 type InputProps = TextareaProps &
   Required<Pick<TextareaProps, 'borderRadius' | 'appearance' | 'full'>>
+
+const getTextareaStyle = (appearance: TextareaAppearance) => (_: {
+  theme: DefaultTheme
+}) => {
+  switch (appearance) {
+    case 'outline':
+      return {
+        resize: 'none' as 'none',
+        '::-webkit-scrollbar': {
+          /* border-width + expected scrollbar width */
+          width: '20px'
+        },
+
+        '::-webkit-scrollbar-track': {
+          boxShadow: `inset 0 0 10px 10px ${_.theme.colors.border.muted}`,
+          border: 'solid 9px transparent'
+        },
+
+        '::-webkit-scrollbar-thumb': {
+          boxShadow: `inset 0 0 10px 10px ${_.theme.colors.border.active}`,
+          border: 'solid 9px transparent'
+        }
+      }
+    default:
+      return {}
+  }
+}
 
 export const Textarea = styled(Text)<InputProps>`
   border: none;
@@ -17,5 +44,7 @@ export const Textarea = styled(Text)<InputProps>`
   ${_ => ({
     width: _.width ? getValueWithUnit(_.width) : _.full ? '100%' : undefined
   })}
+
   ${_ => getTextInputStyle(_.appearance)};
+  ${_ => getTextareaStyle(_.appearance)};
 `
