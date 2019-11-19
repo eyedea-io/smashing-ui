80b52c357d753ab1c337b8f35138feaeb890372c
Mateusz <mateusz.woroniecki@eyedea.io>
2019-11-19 15:51:01 +0100

    feat: add stories

--- stories/button-group.stories.js
+++ stories/button-group.stories.js
@@ -4,6 +4,24 @@ import {ButtonGroup} from '@smashing/button-group'
 import {withA11y} from '@storybook/addon-a11y'
 import {SmashingThemeProvider} from '@smashing/theme'
 
+const options = [
+  {
+    label: 'Lorem Ipsum',
+    value: 1
+  },
+  {
+    label: 'is simply dummy',
+    value: 2
+  },
+  {
+    label: 'text of the printing',
+    value: 3
+  },
+  {
+    label: 'and typesetting industry',
+    value: 4
+  }
+]
 addDecorator(withA11y)
 
 storiesOf('Atomic Elements|Buttons Group', module)
@@ -23,99 +41,86 @@ storiesOf('Atomic Elements|Buttons Group', module)
 
     return (
       <React.Fragment>
+        <p>Default</p>
         <ButtonGroup
           onChange={v => setValue(v)}
           value={value}
-          options={[
-            {
-              label: 'first button',
-              value: 1
-            },
-            {
-              label: 'second button',
-              value: 2
-            }
-          ]}
+          options={options.slice(0, 2)}
         />
-        <br />
+        <p>Flat</p>
         <ButtonGroup
           appearance="flat"
           onChange={v => setValue(v)}
           value={value}
-          options={[
-            {
-              label: 'first button',
-              value: 1
-            },
-            {
-              label: 'second button',
-              value: 2
-            },
-            {
-              label: 'third button',
-              value: 3
-            }
-          ]}
+          options={options.slice(0, 3)}
         />
-        <br />
+        <p>Primary</p>
         <ButtonGroup
           appearance="primary"
           onChange={v => setValue(v)}
           value={value}
-          options={[
-            {
-              label: 'first button',
-              value: 1
-            },
-            {
-              label: 'second button',
-              value: 2
-            },
-            {
-              label: 'third button',
-              value: 3
-            }
-          ]}
+          options={options}
         />
-        <br />
+        <p>Minimal</p>
         <ButtonGroup
           appearance="minimal"
           onChange={v => setValue(v)}
           value={value}
-          options={[
-            {
-              label: 'first button',
-              value: 1
-            },
-            {
-              label: 'second button',
-              value: 2
-            },
-            {
-              label: 'third button',
-              value: 3
-            }
-          ]}
+          options={options}
         />
-        <br />
+        <p>Subtle</p>
         <ButtonGroup
           appearance="subtle"
           onChange={v => setValue(v)}
           value={value}
-          options={[
-            {
-              label: 'first button',
-              value: 1
-            },
-            {
-              label: 'second button',
-              value: 2
-            },
-            {
-              label: 'third button',
-              value: 3
-            }
-          ]}
+          options={options}
+        />
+      </React.Fragment>
+    )
+  })
+  .add('full width', () => {
+    const [value, setValue] = React.useState(1)
+
+    return (
+      <React.Fragment>
+        <p>Default</p>
+        <ButtonGroup
+          full
+          onChange={v => setValue(v)}
+          value={value}
+          options={options.slice(0, 2)}
+        />
+        <p>Flat</p>
+        <ButtonGroup
+          appearance="flat"
+          full
+          onChange={v => setValue(v)}
+          value={value}
+          options={options.slice(0, 3)}
+        />
+        <p>Primary</p>
+        <ButtonGroup
+          appearance="primary"
+          full
+          onChange={v => setValue(v)}
+          value={value}
+          options={options}
+        />
+        <p>Minimal</p>
+        <ButtonGroup
+          appearance="minimal"
+          full
+          onChange={v => setValue(v)}
+          value={value}
+          options={options}
+        />
+        <p>Subtle</p>
+        <ButtonGroup
+          appearance="subtle"
+          full
+          onChange={v => setValue(v)}
+          value={value}
+          options={options}
         />
       </React.Fragment>
     )
