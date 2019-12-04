528e41d0c59b92ec0c5fd51b6e89b5f0f40d661d
Kasper Mikiewicz <IderedPL@gmail.com>
2019-12-04 14:02:02 +0100

    hotfix(theme): export DeepPartial

--- packages/theme/src/index.tsx
+++ packages/theme/src/index.tsx
@@ -18,7 +18,7 @@ export const theme: DefaultTheme = {
 
 export {constants}
 
-type DeepPartial<T> = {
+export type DeepPartial<T> = {
   [P in keyof T]?: T[P] extends Array<infer U>
     ? Array<DeepPartial<U>>
     : T[P] extends ReadonlyArray<infer U>
